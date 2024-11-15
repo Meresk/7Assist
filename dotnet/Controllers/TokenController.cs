363f9121707c0dc238b192731dc683fd6f0aa247
Яков Хватов <YakovX95@mail.ru>
2024-11-16 00:51:46 +0300

    Refactor api

---
+++ dotnet/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using LiveKit.API.Interfaces;
+using LiveKit.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly ILiveKitService _liveKitService;
+
+        public TokenController(ILiveKitService liveKitService)
+        {
+            _liveKitService = liveKitService;
+        }
+
+        [HttpPost]
+        public IActionResult GenerateToken([FromBody] TokenRequest request)
+        {
+            if (string.IsNullOrEmpty(request.RoomName) || string.IsNullOrEmpty(request.ParticipantName))
+            {
+                return BadRequest(new { errorMessage = "roomName and participantName are required" });
+            }
+
+            var token = _liveKitService.CreateLiveKitJWT(request.RoomName, request.ParticipantName);
+            return Ok(new { token });
+        }
+    }
+}
