70930a4d3f1e0710544b26275f44abc613d72840
Яков Хватов <YakovX95@mail.ru>
2024-11-15 22:33:27 +0300

    Added models, dbContext and migrations

---
+++ 7Assist/Migrations/20241115193238_Initial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace _7Assist.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users_lw9_02",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(155)", maxLength: 155, nullable: false),
+                    Surname = table.Column<string>(type: "varchar(155)", maxLength: 155, nullable: false),
+                    Patronymic = table.Column<string>(type: "varchar(155)", maxLength: 155, nullable: false),
+                    Login = table.Column<string>(type: "varchar(155)", maxLength: 155, nullable: false),
+                    Password = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users_lw9_02", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users_lw9_02");
+        }
+    }
+}
---
+++ 7Assist/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _7Assist.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(155)]
+        public string Name { get; set; }
+
+        [Required, StringLength(155)]
+        public string Surname { get; set; }
+
+        [Required, StringLength(155)]
+        public string Patronymic { get; set; }
+
+        [Required, StringLength(155)]
+        public string Login { get; set; }
+
+        [Required, StringLength(255)]
+        public string Password { get; set; }
+    }
+}
